48fd720738384d6370c3ae3253de66330f9c400f
DaviLimaPereira <39305143+DaviLimaPereira@users.noreply.github.com>
2019-06-02 11:02:32 -0300

    Update Classe
    Atualização da classe student

---
+++ src/java/br/com/fatecpg/escola/Student.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fatecpg.escola;
+
+/**
+ *
+ * @author davil
+ */
+public class Student {
+    private long id;
+    private String name;
+    private String address;
+    private String phone;
+
+    public Student(long id, String name, String address, String phone) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+}
